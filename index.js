cd582dce319a700386bca527895eb9fa9a72fdaa
ashlee21salinas <ashlee.salinas24@student.cart.org>
2025-05-22 10:27:37 -0700

    ashlee

---
+++ index.js
@@ -0,0 +1,40 @@
+const list = document.querySelectorAll('.list');
+function activeLink() {
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+    item.addEventListener('click', activeLink));
+
+
+
+
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('dark-light')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
+
+
+const navButton = document.getElementById('nav-bar');
+const navigation = document.querySelector('.navigation');
+
+navButton.addEventListener('click', () => {
+  navigation.classList.toggle('active');
+});

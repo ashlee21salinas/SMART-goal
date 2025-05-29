3317e0b80e5e3de011b6f6ee06e27db43cb570b0
ashlee21salinas <ashlee.salinas24@student.cart.org>
2025-05-29 08:05:56 -0700

    ashlee

--- typing-two.js
+++ typing-two.js
@@ -1,19 +1,21 @@
 const typingText = document.querySelector(".typing-text p"),
-inpField = document.querySelector(".wrapper .input-field"),
-time = document.querySelector(".time span b"),
-mistakeTag = document.querySelector(".mistake span"),
-wpmTag = document.querySelector(".wpm span"),
-cpmTag = document.querySelector(".cpm span");
+    inpField = document.querySelector(".wrapper .input-field"),
+    time = document.querySelector(".time span b"),
+    mistakeTag = document.querySelector(".mistake span"),
+    wpmTag = document.querySelector(".wpm span"),
+    cpmTag = document.querySelector(".cpm span"),
+    tryAgainBtn = document.querySelector("button");
 
 
 let timer,
-maxTime = 60,
-timeLeft = maxTime,
-charIndex = mistakes = isTyping = 0; 
+    maxTime = 60,
+    timeLeft = maxTime,
+    charIndex = mistakes = isTyping = 0;
 
 function randomParagraph() {
     // this gets random number that will always be less than the paragraphs length
     let randIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
     // this gets random text from array and splits all characters
     paragraphs[randIndex].split("").forEach(span => {
         let spanTag = `<span>${span}</span>`;
@@ -26,21 +28,21 @@ function randomParagraph() {
 function initTyping() {
     const characters = typingText.querySelectorAll("span");
     let typedChar = inpField.value.split("")[charIndex];
-    if(charIndex < characters.length - 1 && timeLeft > 0) { // ended right here at 33:19
-        if(!isTyping) { // once timer starts it wont reset every click
+    if (charIndex < characters.length - 1 && timeLeft > 0) { // ended right here at 33:19
+        if (!isTyping) { // once timer starts it wont reset every click
             timer = setInterval(initTimer, 1000)
             isTyping = true;
         }
         // this is for if the user types no character or uses backspace
-        if(typedChar == null) {
+        if (typedChar == null) {
             charIndex--;
             // this is only used if char is incor
-            if(characters[charIndex].classList.contains("incorrect")) {
+            if (characters[charIndex].classList.contains("incorrect")) {
                 mistakes--;
             }
             characters[charIndex].classList.remove("correct", "incorrect");
         } else {
-            if(characters[charIndex].innerText === typedChar) {
+            if (characters[charIndex].innerText === typedChar) {
                 // if user types correct key the adds the correct class other types incorrect
                 characters[charIndex].classList.add("correct");
             } else {
@@ -51,8 +53,8 @@ function initTyping() {
         }
         characters.forEach(span => span.classList.remove("active"));
         characters[charIndex].classList.add("active");
-    
-    
+
+
         let wpm = Math.round((((charIndex - mistakes) / 5) / (maxTime - timeLeft)) * 60);
         // if value is 0, empty, or infinite then it's value is 0
         wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
@@ -60,13 +62,14 @@ function initTyping() {
         wpmTag.innerText = wpm;
         cpmTag.innerText = charIndex - mistakes; // cpm will not count minstakes
     } else {
-
+        inpField.value = "";
+        clearInterval(timer);
     }
 
 }
 
 function initTimer() {
-    if(timeLeft > 0) {
+    if (timeLeft > 0) {
         timeLeft--;
         time.innerText = timeLeft;
     } else {
@@ -74,5 +77,19 @@ function initTimer() {
     }
 }
 
+function resetGame() {
+    // calls loadPragraph function and resets each thing back to 0 on the page
+    randomParagraph();
+    inpField.value = "";
+    clearInterval(timer);
+    timeLeft = maxTime,
+    charIndex = mistakes = isTyping = 0;
+    time.innerText = timeLeft;
+    mistakeTag.innerHTML = mistakes;
+    wpmTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
 randomParagraph();
 inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
